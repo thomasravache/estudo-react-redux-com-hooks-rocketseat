cc30079f940c3ea8b62b4f690e2d4bc648811357
Thomas Ravache <thomasravache31@gmail.com>
2022-05-24 12:16:17 -0300

    feat: criar dispatch que altera estado com base na action

--- src/components/CourseList/index.jsx
+++ src/components/CourseList/index.jsx
@@ -1,18 +1,29 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 function CourseList() {
   const courses = useSelector(state => state.data);
+  const dispatch = useDispatch();
+
+  const addCourse = () => {
+    dispatch({
+      type: 'ADD_COURSE',
+      title: 'Java',
+    });
+  }
 
   return(
-    <ul>
-      {
-        courses.map((course) => (
-          <li key={course}>
-            {course}
-          </li>
-        ))
-      }
-    </ul>
+    <>
+      <ul>
+        {
+          courses.map((course) => (
+            <li key={course}>
+              {course}
+            </li>
+          ))
+        }
+      </ul>
+      <button onClick={addCourse}>Adicionar novo curso</button>
+    </>
   )
 }
 
